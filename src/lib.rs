7578d965f36babfb576faa06fff5d6e4dd9072c8
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2025-10-14 17:15:25 +0700

    Setup workflow

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ use wdk_alloc::WdkAllocator;
 #[global_allocator]
 static GLOBAL_ALLOCATOR: WdkAllocator = WdkAllocator;
 
+use wdk::println;
 use wdk_sys::{NTSTATUS, PCUNICODE_STRING, PDRIVER_OBJECT};
 
 #[unsafe(export_name = "DriverEntry")]
@@ -17,5 +18,16 @@ pub unsafe extern "system" fn driver_entry(
     driver: PDRIVER_OBJECT,
     registry_path: PCUNICODE_STRING,
 ) -> NTSTATUS {
+    let mut driver = unsafe { *driver };
+    driver.DriverUnload = Some(driver_unload);
+
+    let name = driver.DriverName;
+    println!("Loaded driver {name:?} {registry_path:?}");
     0
+}
+
+pub unsafe extern "C" fn driver_unload(driver: PDRIVER_OBJECT) {
+    let driver = unsafe { *driver };
+    let name = driver.DriverName;
+    println!("Unloaded driver {name:?}");
 }
